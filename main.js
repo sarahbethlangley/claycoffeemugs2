d6c10516bacffa189f3c422d8b5e6002b445095a
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2023-03-22 15:56:43 -0500

    done

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = ["Clay", "Clay", "Clay", "Clay"];
+const toFireInKiln = [];
+
+for (lump of clay) {
+  const mug = `${lump} coffee mug`;
+  toFireInKiln.push(mug);
+}
+
+console.log(toFireInKiln);
